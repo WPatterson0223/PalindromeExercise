4401acd64df73563a30a799d2b4fe3d754c1bc36
Will Patterson <wlp223@aol.com>
2023-02-27 11:43:52 -0500

    exercise 1

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+		public bool IsAPalindrome(string str)
+		{
+            char[] charArray = str.ToLower().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray) == str.ToLower();
+            
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("KaYaK", true)]
+        [InlineData("kayak", true)]
+        [InlineData("palindrome", false)]
+        [InlineData("T", true)]
+        public void IsAPalindromeTest(string str, bool expected)
         {
+            var test = new WordSmith();
 
+            bool actual = test.IsAPalindrome(str);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
